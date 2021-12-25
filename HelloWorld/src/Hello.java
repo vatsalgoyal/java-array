6746b1b9b778f048c2e553462d549615385f02a2
Vatsal <vatsalgoyal98@gmail.com>
2021-12-25 14:12:46 +0530

    learning git

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public int i;
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
---
+++ HelloWorld/src/Object.java
@@ -0,0 +1,9 @@
+public class Object {
+    int x =5;
+    public static void main(String args[]){
+        Object myobj = new Object();
+        System.out.println(myobj.x);
+        Hello obj2 = new Hello();
+        System.out.println(obj2.i);
+    }
+}
